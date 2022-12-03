688b84090060599f53366e94c564db6e986a07da
samsonmeseret <samsonmeseretb@gmail.com>
2022-12-03 15:45:51 -0500

    bug fixed on product model

--- model/products.js
+++ model/products.js
@@ -3,15 +3,19 @@ const Mongoose = require("mongoose");
 const productSchema = new Mongoose.Schema(
   {
     name: { type: String, required: [true, "product name must be provided!"] },
-    price: { type: Number, required: [true, "price must be proveded!"] },
+    price: { type: Number, required: [true, "price must be provided!"] },
     featured: { type: Boolean, default: false },
     rating: { type: Number, default: 4.5 },
     company: {
       type: String,
-      enum: {
-        values: ["apple", "dell", "hp", "lenevo"],
-        message: "{VALUE} is not supported",
-      },
+      // enum: {
+      //   values: ["apple", "dell", "hp", "lenevo"],
+      //   message: "{VALUE} is not supported",
+      // },
+    },
+    image: {
+      type: String,
+      required: [true, "image must be provided!"],
     },
   },
   { timestamps: true }
